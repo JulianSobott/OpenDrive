41664097ad51a5b244aabd50b2ce5610f7dbc142
JulianSobott <julian.sobott@gmail.com>
2019-03-07 20:13:24 +0100

    Add possible description at start.py

--- src/OpenDrive/start.py
+++ src/OpenDrive/start.py
@@ -7,3 +7,15 @@
 
 @internal_use:
 """
+
+DESCRIPTION = (
+    "start.py\n"
+    "This script grants you access to OpenDrive.\n"
+    "To view a more detailed description about the tools type: --[argument] --help\n" 
+    "arguments inside '{}' are optional\n"
+    "Following arguments are available:\n\n"
+    "  --help, -h, ?: prints help.\n"
+    "  client       : starts the client background synchronisation\n"
+    "  server       : starts the server\n"
+    "\n"
+)
