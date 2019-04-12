5648692f26fb1d6b071af710738b04c28c9fe879
JulianSobott <julian.sobott@gmail.com>
2019-04-12 21:48:02 +0200

    Update docstring in authentication

--- src/OpenDrive/client_side/authentication.py
+++ src/OpenDrive/client_side/authentication.py
@@ -7,13 +7,19 @@
 public functions
 -----------------
 
+.. autofunction:: login_auto
+.. autofunction:: login_manual
+.. autofunction:: login_manual_user_device_cli
+.. autofunction:: logout
 .. autofunction:: register_user_device
+.. autofunction:: register_user_device_cli
 
-private classes
-----------------
 
 private functions
 ------------------
+
+.. autofunction:: _get_token
+.. autofunction:: _save_received_token
 
 """
 import getpass
@@ -26,6 +32,52 @@ from OpenDrive.client_side import paths
 from OpenDrive.client_side.od_logging import logger
 from OpenDrive.net_interface import server
 from OpenDrive.client_side.interface import Status
+
+
+def register_user_device(username: str, password: str, email: str = None) -> Status:
+    mac_address = get_mac()
+    ret = server.register_user_device(username, password, mac_address, email)
+    if isinstance(ret, str):
+        return Status.fail(ret)
+    else:
+        _save_received_token(ret)
+        return Status.success("Successfully registered")
+
+
+def login_manual(username: str, password: str, allow_auto_login=True) -> Status:
+    if not net_interface.ServerCommunicator.is_connected():
+        print("Can not connect to server. Please try again later")
+        return Status.fail("Can not connect to server. Please try again later.")
+    mac_address = get_mac()
+    ret = server.login_manual_user_device(username, password, mac_address)
+    if isinstance(ret, str):
+        return Status.fail(ret)
+    else:
+        print("Successfully logged in")
+        if allow_auto_login:
+            _save_received_token(ret)
+        return Status.success("Successfully logged in")
+
+
+def logout():
+    server.logout()
+    net_interface.ServerCommunicator.close_connection()
+    return Status.success("Successfully logged out.")
+
+
+def login_auto() -> Status:
+    token = _get_token()
+    if token is not None:
+        mac = get_mac()
+        success = server.login_auto(token, mac)
+        if not success:
+            return Status.fail("Failed to automatically log in.")
+        else:
+            logger.info("Successfully auto logged in")
+            return Status.success("Successfully auto logged in")
+
+    else:
+        return Status.fail("Failed to automatically log in.")
 
 
 def register_user_device_cli() -> None:
@@ -64,21 +116,6 @@ def login_manual_user_device_cli() -> None:
             break
 
 
-def login_auto() -> Status:
-    token = _get_token()
-    if token is not None:
-        mac = get_mac()
-        success = server.login_auto(token, mac)
-        if not success:
-            return Status.fail("Failed to automatically log in.")
-        else:
-            logger.info("Successfully auto logged in")
-            return Status.success("Successfully auto logged in")
-
-    else:
-        return Status.fail("Failed to automatically log in.")
-
-
 def _save_received_token(token: Token) -> None:
     with open(paths.AUTHENTICATION_PATH, "w+") as file:
         file.write(token.token)
@@ -91,34 +128,3 @@ def _get_token() -> Optional[Token]:
             return Token.from_string(token)
     except FileNotFoundError:
         return None
-
-
-def register_user_device(username: str, password: str, email: str = None) -> Status:
-    mac_address = get_mac()
-    ret = server.register_user_device(username, password, mac_address, email)
-    if isinstance(ret, str):
-        return Status.fail(ret)
-    else:
-        _save_received_token(ret)
-        return Status.success("Successfully registered")
-
-
-def login_manual(username: str, password: str, allow_auto_login=True) -> Status:
-    if not net_interface.ServerCommunicator.is_connected():
-        print("Can not connect to server. Please try again later")
-        return Status.fail("Can not connect to server. Please try again later.")
-    mac_address = get_mac()
-    ret = server.login_manual_user_device(username, password, mac_address)
-    if isinstance(ret, str):
-        return Status.fail(ret)
-    else:
-        print("Successfully logged in")
-        if allow_auto_login:
-            _save_received_token(ret)
-        return Status.success("Successfully logged in")
-
-
-def logout():
-    server.logout()
-    net_interface.ServerCommunicator.close_connection()
-    return Status.success("Successfully logged out.")
