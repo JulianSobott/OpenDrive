55b9ff792e6c61fd48557145bbc07045a40224b8
JulianSobott <julian.sobott@gmail.com>
2019-04-16 11:01:31 +0200

    Start setup gui multiple screens

--- src/OpenDrive/client_side/gui/main.py
+++ src/OpenDrive/client_side/gui/main.py
@@ -23,15 +23,41 @@ private functions
 """
 from kivy.app import App
 from kivy.uix.button import Button
+from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.uix.widget import Widget
 
 
-class TestApp(App):
+class ScreenLoginManual(Screen):
+    pass
+
+
+class ScreenRegister(Screen):
+    pass
+
+
+class ScreenExplorer(Screen):
+    pass
+
+
+class BtnLoginManual(Button):
+
+    def on_release(self):
+        OpenDriveApp.screen_manager.current = "screen_explorer"
+
+
+class OpenDriveApp(App):
+    screen_manager = ScreenManager()
+
     def build(self):
-        return Button(text='Hello World')
+        self.screen_manager.switch_to(ScreenLoginManual())
+        OpenDriveApp.screen_manager.add_widget(ScreenLoginManual(name="screen_login_manual"))
+        OpenDriveApp.screen_manager.add_widget(ScreenExplorer(name="screen_explorer"))
+        OpenDriveApp.screen_manager.current = "screen_login_manual"
+        return self.screen_manager
 
 
 def main():
-    TestApp().run()
+    OpenDriveApp().run()
 
 
 if __name__ == '__main__':
