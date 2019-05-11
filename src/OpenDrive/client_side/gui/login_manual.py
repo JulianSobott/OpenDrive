216c58eff5246969e67f013dcfd143f7548e71fa
JulianSobott <julian.sobott@gmail.com>
2019-05-11 15:26:14 +0200

    Get text of tf_username and password at login manual

--- src/OpenDrive/client_side/gui/login_manual.py
+++ src/OpenDrive/client_side/gui/login_manual.py
@@ -1,15 +1,19 @@
-from kivy.uix.button import Button
+from kivy.properties import ObjectProperty
+from kivy.uix.screenmanager import Screen
 
 from OpenDrive.client_side import interface
 from OpenDrive.client_side.od_logging import logger
 from OpenDrive.client_side.gui import screens
 
 
-class BtnLogin(Button):
+class ScreenLoginManual(Screen):
 
-    def on_release(self):
-        username = ""
-        password = ""
+    tf_username = ObjectProperty(None)
+    tf_password = ObjectProperty(None)
+
+    def btn_release_login(self):
+        username = self.tf_username.text
+        password = self.tf_password.text
         status = interface.login_manual(username, password)
         if status.was_successful() or True:
             logger.debug("Successfully logged in")
@@ -17,4 +21,3 @@ class BtnLogin(Button):
         else:
             logger.debug("Login failed TODO")
             logger.debug(status.get_text())
-
