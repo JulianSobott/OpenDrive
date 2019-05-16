9d0aefff0c982c1bca6f319e4ca6de08cbc464e1
Julian <julian.sobott@gmail.com>
2019-05-16 18:11:28 +0200

    client.main: init_json_file, add is_on_event (test pass)

--- src/OpenDrive/client_side/main.py
+++ src/OpenDrive/client_side/main.py
@@ -17,38 +17,47 @@ private functions
 
 """
 import time
+import threading
 
 from OpenDrive.client_side import file_changes as c_file_changes
 from OpenDrive.client_side import net_start as c_net_start
 from OpenDrive.client_side import interface as c_interface
 from OpenDrive.client_side import synchronization as c_synchronization
+from OpenDrive.client_side import file_changes_json as c_json
+from OpenDrive.client_side.od_logging import logger
 
 """After an call to sync appears the program waits for this time, to prevent too frequent update rates."""
 MIN_UPDATE_PAUSE_TIME = 5
+is_on_event = threading.Event()
 
 
 def start():
     """Function that setups everything."""
+    c_json.init_file()
     c_file_changes.start_observing()
     c_net_start.connect()
     status = c_interface.login_auto()
     if not status.was_successful():
         pass    # TODO: open gui login window
     c_synchronization.full_synchronize()
+    is_on_event.set()
     mainloop()
 
 
 def mainloop():
-    while True:
-        print("Waiting...")
+    while is_on_event.is_set():
+        logger.info("Waiting... for changes")
         c_file_changes.sync_waiter.waiter.wait()
-        c_file_changes.sync_waiter.waiter.clear()
-        time.sleep(MIN_UPDATE_PAUSE_TIME)
-        print("Synchronize")
-        c_synchronization.full_synchronize()
+        if is_on_event.is_set():
+            time.sleep(MIN_UPDATE_PAUSE_TIME)
+            c_file_changes.sync_waiter.waiter.clear()
+            logger.info("Synchronizing")
+            c_synchronization.full_synchronize()
 
 
 def shutdown():
+    is_on_event.clear()
+    c_file_changes.sync_waiter.waiter.set()
     c_net_start.close_connection()
 
 
---
+++ src/tests/client_side/test_main.py
@@ -0,0 +1,43 @@
+import unittest
+import threading
+import os
+import time
+
+from OpenDrive.client_side import main
+from OpenDrive.client_side import paths as client_paths
+from OpenDrive.client_side import interface
+
+from OpenDrive.server_side import paths as server_paths
+from tests.client_side.helper_client import h_register_dummy_user_device_client
+from tests.helper_all import h_client_routine, h_start_server_process, h_stop_server_process, \
+    h_clear_init_all_folders, h_create_empty
+
+
+class TestMain(unittest.TestCase):
+
+    def setUp(self) -> None:
+        h_clear_init_all_folders()
+        self._server_process = h_start_server_process()
+        self.folder1_abs_local_path = client_paths.normalize_path(client_paths.LOCAL_CLIENT_DATA, "folder1")
+        h_create_empty(self.folder1_abs_local_path)
+        main.MIN_UPDATE_PAUSE_TIME = 1
+
+    def tearDown(self) -> None:
+        h_stop_server_process(self._server_process)
+
+    @h_client_routine(clear_folders=False)
+    def test_start_logged_in(self):
+        user = h_register_dummy_user_device_client()
+        main_thread = threading.Thread(target=main.start, daemon=True)
+        main_thread.start()
+        time.sleep(1)   # wait till changes.json is created
+
+        interface.add_sync_folder(self.folder1_abs_local_path, "folder1")
+        file_path = os.path.join(self.folder1_abs_local_path, "dummy.txt")
+        with open(file_path, "w") as f:
+            f.write("Hello World")
+        time.sleep(2)   # wait till synchronization finished
+        expected_path = os.path.join(server_paths.get_users_root_folder(user.user_id), "folder1/dummy.txt")
+        self.assertTrue(os.path.exists(expected_path), "dummy file is not pulled to server!")
+        time.sleep(1)   # wait till waiting...
+        main.shutdown()
