7d1adaee27aa19c4b7d64f9ff796eca5d7794eb6
Julian <julian.sobott@gmail.com>
2019-05-16 22:58:13 +0200

    Start explorer browse_server_path with selectable labels

--- src/OpenDrive/client_side/gui/explorer.py
+++ src/OpenDrive/client_side/gui/explorer.py
@@ -1,8 +1,13 @@
-from kivy.properties import ObjectProperty
+from kivy.properties import ObjectProperty, BooleanProperty
+from kivy.uix.behaviors import FocusBehavior
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.popup import Popup
+from kivy.uix.recycleboxlayout import RecycleBoxLayout
+from kivy.uix.recycleview import RecycleView
+from kivy.uix.recycleview.layout import LayoutSelectionBehavior
+from kivy.uix.recycleview.views import RecycleDataViewBehavior
 from kivy.uix.screenmanager import Screen
 from functools import partial
 
@@ -88,4 +93,45 @@ class PopupConfigFolder(Popup):
         self.tf_client_path.text = path
 
     def browse_server_path(self):
-        pass
+        all_server_folders = interface.get_all_remote_folders()
+        all_server_folders = ["folder1", "folder2"]
+        popup_server_folders = Popup()
+        view = FoldersView(all_server_folders)
+        popup_server_folders.add_widget(view)
+        popup_server_folders.open()
+
+
+class FoldersView(RecycleView):
+    def __init__(self, folders: list, **kwargs):
+        super(FoldersView, self).__init__(**kwargs)
+        self.data = [{"text": folder} for folder in folders]
+
+
+class SelectableRecycleBoxLayout(FocusBehavior, LayoutSelectionBehavior, RecycleBoxLayout):
+    pass
+
+
+class SelectableLabel(RecycleDataViewBehavior, Label):
+    index = None
+    selected = BooleanProperty(False)
+    selectable = BooleanProperty(True)
+
+    def refresh_view_attrs(self, rv, index, data):
+        ''' Catch and handle the view changes '''
+        self.index = index
+        return super(SelectableLabel, self).refresh_view_attrs(rv, index, data)
+
+    def on_touch_down(self, touch):
+        ''' Add selection on touch down '''
+        if super(SelectableLabel, self).on_touch_down(touch):
+            return True
+        if self.collide_point(*touch.pos) and self.selectable:
+            return self.parent.select_with_touch(self.index, touch)
+
+    def apply_selection(self, rv, index, is_selected):
+        ''' Respond to the selection of items in the view. '''
+        self.selected = is_selected
+        if is_selected:
+            print("selection changed to {0}".format(rv.data[index]))
+        else:
+            print("selection removed for {0}".format(rv.data[index]))
