c9674ce8ebd33aceed557b08d4d74e691a5c9ee2
Julian <julian.sobott@gmail.com>
2019-05-20 12:16:51 +0200

    Fix Typo from path client_Side to client_side

--- src/OpenDrive/client_side/paths.py
+++ src/OpenDrive/client_side/paths.py
@@ -11,7 +11,7 @@ import os
 
 from OpenDrive.general.paths import *
 
-LOCAL_CLIENT_DATA = os.path.join(LOCAL_DATA, "client_Side")
+LOCAL_CLIENT_DATA = os.path.join(LOCAL_DATA, "client_side")
 
 LOCAL_DB_PATH = os.path.join(LOCAL_CLIENT_DATA, "local_data.db")
 LOCAL_JSON_PATH = os.path.join(LOCAL_CLIENT_DATA, "changes.json")
