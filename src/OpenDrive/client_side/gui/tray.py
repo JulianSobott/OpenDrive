ce13419b6db7c1d922a4cf7c5f01d4b72ebcce82
Julian <julian.sobott@gmail.com>
2019-10-23 21:34:33 +0200

    add functionality to close tray

--- src/OpenDrive/client_side/gui/tray.py
+++ src/OpenDrive/client_side/gui/tray.py
@@ -25,6 +25,8 @@ from OpenDrive.client_side import gui
 from OpenDrive.client_side.od_logging import logger
 from OpenDrive.client_side import paths as c_paths
 
+tray = None
+
 
 def _open_gui():
     gui.main.main()
@@ -32,16 +34,37 @@ def _open_gui():
 
 def start_tray(background_function, shutdown_function):
     """Add an tray icon and is blocking the program. Must be called from the main thread."""
+    global tray
+
     def background_wrapper(icon_):
         background_function()
 
+    def _stop():
+        """
+        Stops synchronization. Tray keeps open
+        """
+        shutdown_function()
+
+    def _close():
+        """
+        Stops synchronization and stops the tray.
+        """
+        shutdown_function()
+        stop_tray()
+
     image = PIL.Image.open(c_paths.normalize_path(c_paths.ASSETS, "Logo.png"))
-    menu = pystray.Menu(pystray.MenuItem("Open", _open_gui), pystray.MenuItem("Stop", shutdown_function))
-    icon = pystray.Icon("OpenDrive", image, "OpenDrive", menu)
-    icon.icon = image
-    icon.visible = True
+    menu = pystray.Menu(pystray.MenuItem("Open", _open_gui), pystray.MenuItem("Stop", _stop),
+                        pystray.MenuItem("Close", _close))
+    tray = pystray.Icon("OpenDrive", image, "OpenDrive", menu)
+    tray.icon = image
+    tray.visible = True
     logger.info("Tray started")
-    icon.run(background_wrapper)
+    tray.run(background_wrapper)
+
+
+def stop_tray():
+    global tray
+    tray.stop()
 
 
 if __name__ == '__main__':
