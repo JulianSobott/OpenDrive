6ad2303bba0f4ac396ec613f82a087a66eb34d49
Julian Sobott <julian.sobott@gmail.com>
2019-11-30 20:58:43 +0100

    [feature] Add new loggers at client side

--- src/OpenDrive/client_side/od_logging.py
+++ src/OpenDrive/client_side/od_logging.py
@@ -1,21 +1,34 @@
 """
 @author: Julian Sobott
 @created: 13.11.2018
-@brief:
-@description:
 
-@external_use:
+Available loggers:
 
-@internal_use:
-
+- **logger_general:** Logging the program flow
+- **logger_network:** Logging network stuff
+- **logger_sync:** Logging synchronization stuff
 """
 import logging
 
-logger = logging.getLogger("Client")
-handler = logging.StreamHandler()
-formatter = logging.Formatter(
-        '%(levelname)-8s %(message)s \t\t(%(filename)s %(lineno)d %(name)s)')
-handler.setFormatter(formatter)
-logger.addHandler(handler)
-logger.setLevel(logging.DEBUG)
 
+def setup_logger(logger_name, log_file=None, level=logging.INFO):
+    new_logger = logging.getLogger(logger_name)
+    formatter = logging.Formatter(
+        '[%(levelname)-8s] [%(name)-7s] %(message)s \t\t [%(asctime)s] (%(filename)s %(lineno)d %(name)s) (%('
+        'threadName)s)')
+    if log_file:
+        file_handler = logging.FileHandler(log_file, mode="w")
+        file_handler.setFormatter(formatter)
+        new_logger.addHandler(file_handler)
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+    new_logger.addHandler(stream_handler)
+
+    new_logger.setLevel(level)
+    return new_logger
+
+
+logger = setup_logger("Client")     # Deprecated: Replace with new ones
+logger_general = setup_logger("General")
+logger_network = setup_logger("Network")
+logger_sync = setup_logger("Sync")
