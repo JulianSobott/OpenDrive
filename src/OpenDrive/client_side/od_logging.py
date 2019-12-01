56627eb840bd9d1127b5d6cd0812057e8eb261ea
Julian Sobott <julian.sobott@gmail.com>
2019-12-01 11:34:55 +0100

    [feature] Move setup_logger to general

--- src/OpenDrive/client_side/od_logging.py
+++ src/OpenDrive/client_side/od_logging.py
@@ -12,26 +12,9 @@ Available loggers:
 """
 import logging
 import pynetworking
+from OpenDrive.general.od_logging import setup_logger
 
 pynetworking.Logging.logger.setLevel(logging.WARNING)
-
-
-def setup_logger(logger_name, log_file=None, level=logging.INFO):
-    new_logger = logging.getLogger(logger_name)
-    new_logger.handlers = []
-    formatter = logging.Formatter(
-        '[%(levelname)-8s] [%(name)-7s] %(message)s \t\t [%(asctime)s] (%(filename)s %(lineno)d %(name)s) (%('
-        'threadName)s)')
-    if log_file:
-        file_handler = logging.FileHandler(log_file, mode="w")
-        file_handler.setFormatter(formatter)
-        new_logger.addHandler(file_handler)
-    stream_handler = logging.StreamHandler()
-    stream_handler.setFormatter(formatter)
-    new_logger.addHandler(stream_handler)
-
-    new_logger.setLevel(level)
-    return new_logger
 
 
 logger_gui = setup_logger("GUI")
