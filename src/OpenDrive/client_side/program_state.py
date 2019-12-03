94175183987348d34813784084bc89eb99f86456
Julian Sobott <julian.sobott@gmail.com>
2019-12-03 22:27:16 +0100

    [feature] Add program_state module for global client data

---
+++ src/OpenDrive/client_side/program_state.py
@@ -0,0 +1,9 @@
+"""
+:module: OpenDrive.client_side.program_state
+:synopsis: Global program states
+:author: Julian Sobott
+
+"""
+
+#: Requests should only be made when this is True
+is_authenticated_at_server = False
